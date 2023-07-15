988136a5d0879684f3a143ac634f2dcbdd5dc619
saurabh thakur <st0857910@gmail.com>
2023-07-15 16:29:58 +0530

    Added more code

--- src/test/java/forgetPass.java
+++ src/test/java/forgetPass.java
@@ -22,13 +22,50 @@ public class forgetPass {
 		
 		driver.manage().window().maximize();
 		
-      driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+      driver.get("https://resumegenius.com/dashboard/app/account/login");
       
-      driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
-		
-    String text=driver.findElement(By.xpath("//h6[normalize-space()='Reset Password']")).getText();
-    
-    Assert.assertEquals(text, "Reset Password");
+      driver.findElement(By.cssSelector(".text-link")).click();
+       
+      
+      driver.findElement(By.cssSelector(".EMAI")).sendKeys("st0851097@gmail.com");
+      
+      driver.findElement(By.cssSelector(".reset-button button")).click();
+      
+      
+      String SuccessMassage = driver.findElement(By.cssSelector(".builder-form-layout-title")).getText();
+      
+      Assert.assertEquals("Success", SuccessMassage);
+      
+     
+      
+      
+      
+      
+      
 }
-
+	
+	
+	
+	@Test
+	public void loginAgain() throws InterruptedException 
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver =new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+		
+		driver.manage().window().maximize();
+		
+      driver.get("https://resumegenius.com/dashboard/app/account/login");
+	
+      
+      driver.findElement(By.cssSelector(".EMAI")).sendKeys("st0851097@gmail.com");
+      
+      driver.findElement(By.cssSelector(".PSWD")).sendKeys("Qwerty@1234");
+      
+      driver.findElement(By.cssSelector(".btn-group-footer button")).click();
+      
+      
+	
+	}
 }
